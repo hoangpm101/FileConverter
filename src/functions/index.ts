c0476d37808b17ab3074979b6bb330ceecb33b23
Phan Minh Hoang <hoangpm22@gmail.com>
2022-07-09 16:43:42 +0700

    test fix

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,7 +1,6 @@
-import getProperty from './getProperty'
 import csvToJSON from './csvToJSON'
 import jsonToCSV from './jsonToCSV'
 import webmToMP4 from './webmToMP4'
 import mp4ToWebm from './mp4ToWebm'
 
-export { getProperty, csvToJSON, jsonToCSV, webmToMP4, mp4ToWebm }
+export { csvToJSON, jsonToCSV, webmToMP4, mp4ToWebm }
